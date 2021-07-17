16d8e44053c0983f2e70516238d5758d86a305ea
Sebastien Pierre-Jacques <sebastienpj@protonmail.com>
2021-07-17 16:11:31 -0500

    Added styling, fixed multiple operator bug

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const buttons = document.querySelectorAll(".button");
 const resultDisplay = document.querySelector(".result");
 
+
 console.log(buttons);
 
-// let contentArray = [];
+let charactersHistory = [];
 let firstNumberDone = false;
 let operatorUsed = "";
 let operatorCount = 0;
@@ -12,33 +13,42 @@ let num2 = "";
 let answer = 0;
 let periodUsed = false;
 
+
 buttons.forEach((button) => button.addEventListener("click", calculator));
 
 document.addEventListener("keydown", keyboard);
 
 
 
-
-
 /******************* DOM EVENTS **************************/
 
 function calculator(objectPressed) {
+
+  console.log(objectPressed);
+
   let keyValue = objectPressed.target.innerText;
   let objectClasses = objectPressed.target.classList;
 
+  charactersHistory.push(keyValue);
+  console.log(charactersHistory);
+
+
+  buttonAnimation(objectPressed)
   
-  if(firstNumberDone) {
-    if (objectClasses.contains("number") || (objectClasses.contains("period") && periodUsed == false)){
+
+  if (firstNumberDone) {
+
+    if (objectClasses.contains("number") || (objectClasses.contains("period") && periodUsed == false)) {
       num2 += keyValue;
       updateDisplay(num2);
     }
+
   } else {
-    if (objectClasses.contains("number") || (objectClasses.contains("period") && periodUsed == false)){
+    if (objectClasses.contains("number") || (objectClasses.contains("period") && periodUsed == false)) {
       num1 += keyValue;
       updateDisplay(num1);
     }
   }
-
 
   switch (keyValue) {
     case ".":
@@ -52,19 +62,21 @@ function calculator(objectPressed) {
     break;
 
     case "-":
-      updateOperatorCount()
+      updateOperatorCount();
       operatorUsed = "substract";
       startSecondNumber();
     break;
 
-    case "/":
-      updateOperatorCount()
+    case "/":      
+      console.log(num1);
+      console.log(Number(num2));
+      updateOperatorCount();
       operatorUsed = "divide";
       startSecondNumber();
     break;
 
     case "*":
-      updateOperatorCount()
+      updateOperatorCount();
       operatorUsed = "multiply";
       startSecondNumber();
     break;
@@ -74,7 +86,6 @@ function calculator(objectPressed) {
       updateDisplay(answer);
       // num1 = answer;
       // // num2 = 0;
-
     break;
 
     case "Clear":
@@ -86,182 +97,170 @@ function calculator(objectPressed) {
       answer = 0;
       operatorUsed = "";
       operatorCount = 0;
+      charactersHistory = [];
     break;
 
     case "Delete":
-      firstNumberDone ? num2 = deleteLastCharacter(num2) : num1 = deleteLastCharacter(num1);
+      firstNumberDone ? (num2 = deleteLastCharacter(num2)) : (num1 = deleteLastCharacter(num1));
     break;
 
     default:
     break;
   }
 
+  console.log(`Operator Count: ${operatorCount}`);
 }
 
-
-
-
-
 /*********************** GLOBAL FUNCTIONS ************************/
+function buttonAnimation(currentKey){
+ let activeButton = currentKey.target; 
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+      activeButton.classList.remove("pressed");
+  }, 200);
+
+}
+
 
 function updateDisplay(value) {
   resultDisplay.innerText = value;
 }
 
-
-
-
-
 /*** Finds answer for stringing multiple operators,
 updates operator count ***/
+function updateOperatorCount() {  
 
-function updateOperatorCount() {
-  operatorCount += 1
+  switch (charactersHistory[charactersHistory.length - 2]) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9": 
+    case ".":
+      operatorCount += 1;
 
-  if (operatorCount > 1) {
-    getAnswer(operatorUsed, Number(num1), Number(num2));
-    num1 = answer;    
-    num2 = "";    
-    updateDisplay(num1);
+      if (operatorCount > 1) {
+        getAnswer(operatorUsed, Number(num1), Number(num2));
+        num1 = answer;
+        num2 = "";
+        updateDisplay(num1);
+      }
+
+    break;
+  
+    default:
+    break;
   };
-
-}
-
+};
 
 
 
 function getAnswer(sign, firstNum, secondNum) {
   answer = operate(sign, firstNum, secondNum);
-}
-
-
-
+};
 
 
 function operate(operator, firstNumber, secondNumber) {
   switch (operator) {
-    case "add":      
+    case "add":
       return firstNumber + secondNumber;
+
     break;
 
     case "substract":
       return firstNumber - secondNumber;
+
     break;
 
     case "multiply":
       return firstNumber * secondNumber;
+
     break;
 
     case "divide":
       return firstNumber / secondNumber;
+
     break;
 
     default:
       console.log("Something went wrong in the operate function");
+
     break;
-  }
-}
-
-
-
+  };
+};
 
 function startSecondNumber() {
   firstNumberDone = true;
   periodUsed = false;
-}
-
-
-
+};
 
 function deleteLastCharacter(number) {
   let newNumber = number.slice(0, number.length - 1);
   updateDisplay(newNumber);
   return newNumber;
-}
-
-
-
+};
 
 /******************* KEYBOARD EVENTS *********************/
 
 function keyboard(buttonPressed) {
-
   console.log(buttonPressed);
 
+  charactersHistory.push(buttonPressed.key);
+  console.log(charactersHistory);
 
   switch (buttonPressed.key) {
-    case "1":
-      updateNumber(buttonPressed.key);
-      break;
-
-    case "2":
-      updateNumber(buttonPressed.key);
-      break;
-
-    case "3":
-      updateNumber(buttonPressed.key);
-      break;
-
-    case "4":
-      updateNumber(buttonPressed.key);
-      break;
-
-    case "5":
-      updateNumber(buttonPressed.key);
-      break;
-
-    case "6":
-      updateNumber(buttonPressed.key);
-      break;
-
-    case "7":
-      updateNumber(buttonPressed.key);
-      break;
-
-    case "8":
-      updateNumber(buttonPressed.key);
-      break;
-
-    case "9":
-      updateNumber(buttonPressed.key);
-      break;
-
     case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":  
       updateNumber(buttonPressed.key);
-      break;
+    break;
 
     case ".":
       if (periodUsed == false) {
         updateNumber(buttonPressed.key);
       };
+
       periodUsed = true;
-
-      break;
-
-
+    break;
 
     case "+":
       updateOperatorCount();
       operatorUsed = "add";
       startSecondNumber();
+
     break;
 
     case "-":
-      updateOperatorCount()
+      updateOperatorCount();
       operatorUsed = "substract";
       startSecondNumber();
+
     break;
 
     case "/":
-      updateOperatorCount()
+      updateOperatorCount();
       operatorUsed = "divide";
       startSecondNumber();
+
     break;
 
     case "*":
-      updateOperatorCount()
+      updateOperatorCount();
       operatorUsed = "multiply";
       startSecondNumber();
+
     break;
 
     case "=":
@@ -274,9 +273,8 @@ function keyboard(buttonPressed) {
     break;
 
     case "Backspace":
-      firstNumberDone ? num2 = deleteLastCharacter(num2) : num1 = deleteLastCharacter(num1);
+      firstNumberDone ? (num2 = deleteLastCharacter(num2)) : (num1 = deleteLastCharacter(num1));
     break;
-
 
     case "c":
       updateDisplay(0);
@@ -287,29 +285,25 @@ function keyboard(buttonPressed) {
       answer = 0;
       operatorUsed = "";
       operatorCount = 0;
+      charactersHistory = [];
     break;
 
     default:
-      break;
+    break;
   };
 
-
-
-
-  function updateNumber(value){
+  function updateNumber(value) {
     if (firstNumberDone) {
-
       num2 += value;
+
       updateDisplay(num2);
-
     } else {
-
       num1 += value;
-      updateDisplay(num1);
 
+      updateDisplay(num1);
     };
   };
 
-
+  console.log(`Operator Count: ${operatorCount}`);
 
 };
